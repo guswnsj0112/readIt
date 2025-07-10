37c1b468c2da5ed86d58cb09a34279049c6b1263
guswnsj0112 <guswnsj0112@naver.com>
2025-07-10 13:39:20 +0000

    랜딩 페이지 완성

--- readit-project/src/components/nav.jsx
+++ readit-project/src/components/nav.jsx
@@ -1,16 +1,25 @@
 import nav_logo from '../images/nav_logo.png';
-import './nav.css'
+import './nav.css';
 
 function Nav() {
   return (
-    <div className="Nav">
-		  <img src={nav_logo}  alt="로고 사진" width="250" height="75" className ="navLogo"/>
-		  <ul className="menu">
-			  <li>소개 <p>01</p></li>
-			  <li>책장 <p>02</p></li>
-			  <li>문의 <p>03</p></li>
-		  </ul>
-    </div>
+    <nav className="Nav">
+      <img src={nav_logo} alt="로고 사진" className="navLogo" />
+      <ul className="menu">
+        <li>
+          <span>소개</span>
+          <p>01</p>
+        </li>
+        <li>
+          <span>책장</span>
+          <p>02</p>
+        </li>
+        <li>
+          <span>문의</span>
+          <p>03</p>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
