2a553e19676649f995679eefc2591530bd7847a5
장현준 <guswnsj0112@naver.com>
2025-07-27 06:21:51 +0000

    Home.jsx에 Router 적용

--- read-it/src/App.jsx
+++ read-it/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Home from './pages/Home';
-
+import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
 		  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
-		  <Home />
+		  <Routes>
+  			<Route path="/" element={<Home />} />
+		  </Routes>
     </div>
   );
 }
