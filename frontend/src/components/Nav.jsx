1e4bc49a1ade6efe3a86d254e0d0825d69e5b3af
장현준 <guswnsj0112@naver.com>
2025-08-02 13:50:31 +0000

    fix: NavBar 여백 수정

---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,29 @@
+import nav_logo from "../images/nav_logo.png";
+import "./Nav.css";
+import { Link } from "react-router-dom";
+function Nav() {
+  return (
+    <nav className="Nav">
+		<Link to="/">
+		  <img src={nav_logo} alt="로고 사진" className="navLogo" />
+		 </Link>
+      <ul className="menu">
+        <li>
+          <Link to="/">
+            <span> 소개 <sup>01</sup> </span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/bookcase">
+            <span> 책장 <sup>02</sup> </span>
+          </Link>
+        </li>
+        <li>
+          <span> 문의 <sup>03</sup> </span>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
