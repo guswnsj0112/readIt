d9825390cece31e8b61028f0e1bcac98249ffafa
장현준 <guswnsj0112@naver.com>
2025-09-21 06:48:56 +0000

    chore: 구름ide 호스팅 주소 수정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     port: 3000,
     hmr: {
       protocol: "wss",
-      host: "readit-alcvm.run.goorm.io",
+      host: "readit-jrklk.run.goorm.io",
       clientPort: 443
     },
     proxy: {
